1044a48991c742920c8690bc92bf26d4e2484896
Craig A. Butler <craig@Craigs-Ad-Hoc-MBP.local>
2018-05-04 17:21:31 -0400

    Adding link URL to CSV

--- scripts/migration_status_update.py
+++ scripts/migration_status_update.py
@@ -25,6 +25,7 @@ def createDashboardCSV(repo, markdown_files):
     output_file = os.path.join(os.environ['DATA_DIR'], 'migration_status.csv')
     with open(output_file, 'w') as migration_status:
         migration_status.write('name,\
+                                link,\
                                 lead,\
                                 pre_intake,\
                                 oit_intake,\
@@ -37,17 +38,19 @@ def createDashboardCSV(repo, markdown_files):
         for md in markdown_files:
             if md.casefold() not in IGNORE_LIST:
                 full_path = MARKDOWN_DIR + '/' + md
+                doc_path = repo.html_url + '/' + full_path
                 document = repo.file_contents(full_path).decoded.decode('utf-8')
-                product_rows.append(docToRow(document))
+                product_rows.append(docToRow(document, doc_path))
         product_rows.sort()
         for row in product_rows:
             migration_status.write(row + "\n")
 
 
-def docToRow(document):
+def docToRow(document, doc_path):
     lines = document.splitlines()
     lines = list(filter(lambda x: not re.match(r'^\s*$', x), lines))
     product_name = lines[0].split(":")[1].strip()
+    product_link = doc_path
     product_lead = lines[1].split(":")[1].strip()
     migrate_to_cloud = lines[2].split(":")[1].strip()
     cutover_complete = lines[3].split(":")[1].strip()
@@ -64,7 +67,7 @@ def docToRow(document):
         elif "COMPLETE ALL Decommission Tasks" in line:
             decom = get_status(line)
 
-    return ",".join((product_name, product_lead, pre_intake,
+    return ",".join((product_name, product_link, product_lead, pre_intake,
                      oit_intake, migrate_to_cloud, migration_planning,
                      migration_cutover, cutover_complete, decom))
 
