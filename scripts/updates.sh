49e0bef4cd240ab149fc1ea0520c89bbc07c4dcc
Shefali Nayak <shefali.nayak@thoughtworks.com>
2020-03-03 14:32:38 -0500

    [Shefali/Phil] enables running python scripts locally

--- scripts/updates.sh
+++ scripts/updates.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-python /application/google_analytics/update_data.py
-python /application/google_analytics/update_counts.py
+python google_analytics/update_data.py
+python google_analytics/update_counts.py
 
-python /application/idme/update_accounts.py
+python idme/update_accounts.py
 
 #python /application/prometheus/update_from_prometheus.py
 
-python /application/migration_status_update.py
+python migration_status_update.py
