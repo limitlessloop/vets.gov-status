afd5512946a6acb9cbfec83e04f3890548f4176d
Shefali Nayak <shefali.nayak@thoughtworks.com>
2020-03-04 12:14:33 -0500

    [Shefali/Katie] Adds script to run python scripts locally

---
+++ scripts/run_updates_local.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+export DATA_DIR="data"
+export CONFIG_DIR=""
+export GA_SERVICEACCOUNT="serviceaccount.json"
+export MARKDOWN_DIR="migration_status"
+
+python google_analytics/update_data.py
+python google_analytics/update_counts.py
+
+python idme/update_accounts.py
+
+#python /application/prometheus/update_from_prometheus.py
+
+python migration_status_update.py
