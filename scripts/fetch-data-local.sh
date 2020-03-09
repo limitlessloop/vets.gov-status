dea9ee4dd0be0f39e5d07175faca6d6fad83b8ce
Elvin Verdiyev <elvin.verdiyev@thoughtworks.com>
2020-03-09 14:18:45 -0400

    more check

--- scripts/fetch-data-local.sh
+++ scripts/fetch-data-local.sh
@@ -6,7 +6,7 @@ export GA_SERVICEACCOUNT="serviceaccount.json"
 export MARKDOWN_DIR="migration_status"
 
 python google_analytics/update_data.py
-python google_analytics/update_counts.py
+#python google_analytics/update_counts.py
 
 python idme/update_accounts.py
 
