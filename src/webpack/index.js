3bd7e2e6b5da61808dd259a01335ebd5d89cfe18
Phil Herbert <phil.a.herbert@gmail.com>
2020-03-12 13:47:12 -0400

    initial webpack stuff - fonts and svgs currently broken

---
+++ src/webpack/index.js
@@ -0,0 +1,3 @@
+import "@department-of-veterans-affairs/formation/dist/formation";
+import "@department-of-veterans-affairs/formation/dist/formation.min.css";
+import Chart from "chart.js"
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/webpack/index.js',
+  output: {
+    path: path.join(__dirname, 'src/assets/vendor'),
+    filename: 'js/bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      // images
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'img/'
+          }
+        }
+      },
+      // fonts
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/'
+          }
+        }
+      }
+    ]
+  }
+};
