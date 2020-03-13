b93c7a5935b278f8c3c4c984c459d2e36db09ce7
Karl Brown <kabrown+va@thoughtworks.com>
2020-03-13 01:07:25 -0400

    [#3205] Work around behavioral difference between BSD/GNU cp commands

--- copy-node-dependencies.sh
+++ copy-node-dependencies.sh
@@ -9,4 +9,5 @@ mkdir -p src/assets/vendor/chart.js
 cp node_modules/chart.js/dist/Chart.bundle.min.js src/assets/vendor/chart.js/
 
 mkdir -p src/assets/vendor/formation
-cp -r node_modules/@department-of-veterans-affairs/formation/dist/ src/assets/vendor/formation/
+# The "*" after /dist/ is important here, as BSD vs GNU treats the semantics differently if you don't have the *
+cp -r node_modules/@department-of-veterans-affairs/formation/dist/* src/assets/vendor/formation/
