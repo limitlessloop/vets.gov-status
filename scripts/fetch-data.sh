f41781be45acbbfb98ba70f33fa3822ef9d43475
Karl Brown <kabrown+va@thoughtworks.com>
2020-03-12 21:54:03 -0400

    [#3198] Add more tracing to fetch-data script

--- scripts/fetch-data.sh
+++ scripts/fetch-data.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -o errexit
 set -o nounset
+set -x
 
 # todo: Make credentials key an environment variable as well
+printenv
 credstash --region ${AWS_DEFAULT_REGION} get vagovanalytics.prod.service_account_credentials > ${GA_SERVICEACCOUNT}
 python google_analytics/update_data.py
 python google_analytics/update_counts.py
