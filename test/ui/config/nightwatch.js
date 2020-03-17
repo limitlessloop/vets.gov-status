a9474c01ce36d3906c7e9b6af7e93c92fa02716e
Filip Fafara <ffafara@thoughtworks.com>
2020-03-17 14:54:45 -0400

    [#3227] make docker containers talk to each other

--- test/ui/config/nightwatch.js
+++ test/ui/config/nightwatch.js
@@ -1,72 +1,74 @@
 /* eslint-disable camelcase, strict */
-'use strict';
+"use strict";
 
 // const fs = require('fs-extra');
 // const seleniumServer = require('selenium-server');
 
-require('@babel/register');
-require('@babel/polyfill');
+require("@babel/register");
+require("@babel/polyfill");
 
 // const selenium_logs = './logs/selenium';
 const selenium_server_port = 4444;
-const selenium_host = 'selenium';
+const selenium_host = process.env.selenium_host;
+const jekyll_port = 4000;
+const jekyll_host = process.env.jekyll_host;
 
 // fs.ensureDirSync(selenium_logs);
 
 module.exports = {
-  output_folder: './logs/nightwatch',
-  src_folders: './',
-  custom_commands_path: './nightwatch-commands/',
-  live_output: true,
-  parallel_process_delay: 10,
-  disable_colors: process.env.BUILDTYPE === 'production',
-  test_workers: false,
-  test_settings: {
-    default: {
-      launch_url: `jekyll:4000`,
-      filter: '**/*.e2e.spec.js',
-      selenium_host: selenium_host,
-      selenium_port: selenium_server_port,
-      use_ssl: false,
-      silent: true,
-      output: true,
-      screenshots: {
-        enabled: true,
-        on_failure: true,
-        path: 'logs/screenshots',
-      },
-      desiredCapabilities: {
-        browserName: 'chrome',
-        javascriptEnabled: true,
-        acceptSslCerts: true,
-        webStorageEnabled: true,
-        chromeOptions: {
-          args: ['--window-size=1024,768'],
-          w3c: false
+    output_folder: "./logs/nightwatch",
+    src_folders: "./",
+    custom_commands_path: "./nightwatch-commands/",
+    live_output: true,
+    parallel_process_delay: 10,
+    disable_colors: process.env.BUILDTYPE === "production",
+    test_workers: false,
+    test_settings: {
+        default: {
+            launch_url: `${jekyll_host}:${jekyll_port}`,
+            filter: "**/*.e2e.spec.js",
+            selenium_host: selenium_host,
+            selenium_port: selenium_server_port,
+            use_ssl: false,
+            silent: true,
+            output: true,
+            screenshots: {
+                enabled: true,
+                on_failure: true,
+                path: "logs/screenshots"
+            },
+            desiredCapabilities: {
+                browserName: "chrome",
+                javascriptEnabled: true,
+                acceptSslCerts: true,
+                webStorageEnabled: true,
+                chromeOptions: {
+                    args: ["--window-size=1024,768"],
+                    w3c: false
+                }
+            },
+            selenium: {
+                start_process: false,
+                // log_path: selenium_logs,
+                host: selenium_host,
+                port: selenium_server_port
+            },
+            test_workers: {
+                enabled: false,
+                workers: parseInt(process.env.CONCURRENCY || 1, 10)
+            }
         },
-      },
-      selenium: {
-        start_process: false,
-        // log_path: selenium_logs,
-        host: selenium_host,
-        port: selenium_server_port,
-      },
-      test_workers: {
-        enabled: false,
-        workers: parseInt(process.env.CONCURRENCY || 1, 10),
-      },
-    },
-    headless: {
-      desiredCapabilities: {
-        chromeOptions: {
-          args: ['--headless', '--window-size=1024,768'],
+        headless: {
+            desiredCapabilities: {
+                chromeOptions: {
+                    args: ["--headless", "--window-size=1024,768"]
+                }
+            }
         },
-      },
-    },
-    bestpractice: {
-      globals: {
-        rules: ['section508', 'wcag2a', 'wcag2aa', 'best-practice'],
-      },
-    },
-  },
+        bestpractice: {
+            globals: {
+                rules: ["section508", "wcag2a", "wcag2aa", "best-practice"]
+            }
+        }
+    }
 };
