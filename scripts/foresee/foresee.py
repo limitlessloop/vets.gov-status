f53b37a7371f0552fe490babe2e4c497b2cccd3d
saman.moshafi <saman.moshafi@thoughtworks.com>
2020-03-17 16:24:19 -0400

    [T166][Saman/Filip] connect to foresee, keep the data and calculate monthly as well as average of last year

--- scripts/foresee/foresee.py
+++ scripts/foresee/foresee.py
@@ -1,6 +1,8 @@
 from os import environ
 import requests
 import logging
+from scripts.datehelpers import find_last_twelve_months
+
 
 def authenticate():
     url = "https://api.foresee.com/v1/token"
@@ -10,7 +12,7 @@ def authenticate():
     headers = {
         'accept': "application/json",
         'content-type': "application/json",
-        'authorization': "Basic "+ environ.get('FORSEE_CREDENTIALS')
+        'authorization': "Basic " + environ.get('FORSEE_CREDENTIALS')
     }
 
     response = requests.request("POST", url, headers=headers, params=querystring)
@@ -19,6 +21,7 @@ def authenticate():
         raise PermissionError
 
     return response.json()['access_token']
+
 
 def get_measure_data(bearer_token, measure, from_date, to_date):
     offset = 0
@@ -40,7 +43,7 @@ def get_measure_data(bearer_token, measure, from_date, to_date):
     }
     measure_data = []
     has_more = True
-    
+
     while has_more:
         logging.info("Downloading page: {:n} ".format(offset))
         response = requests.request("GET", url, headers=headers, params=querystring)
@@ -51,22 +54,52 @@ def get_measure_data(bearer_token, measure, from_date, to_date):
         offset += 1
         querystring['offset'] = str(offset)
         measure_data.extend(response_json['items'])
-    
-    print(measure_data)
+
+    logging.debug(measure_data)
     return measure_data
 
+
 def calculate_average_satisfaction(items):
-    extracted_csats =  (next(latent_score['score'] for latent_score in item['latentScores'] if latent_score['name'] == 'Satisfaction') for item in items)
+    extracted_csats = (next(latent_score['score'] for latent_score in item['latentScores'] if latent_score['name'] == 'Satisfaction') for item in items)
     return sum(extracted_csats) / len(items)
-    
+
+
+def fetch_last_12_months_data():
+    last_year_data = []
+    last_twelve_months = find_last_twelve_months()
+
+    measure_id = "8847572"
+    bearer_token = authenticate()
+
+    # get values for the last 12 months and calculate average for each month
+    for start_end_date in last_twelve_months:
+        month_data = get_measure_data(bearer_token, measure_id, start_end_date[0].isoformat(),
+                                      start_end_date[1].isoformat())
+        month_year_text = str(start_end_date[0].month) + '/' + str(start_end_date[0].year)
+        one_month_dict = {
+            'month': month_year_text,
+            'month_data': month_data,
+            'csat_score': calculate_average_satisfaction(month_data)
+        }
+        last_year_data.append(one_month_dict)
+        logging.info("Calculated %s average: %.2f", month_year_text, one_month_dict['cast_score'])
+    return last_year_data
+
+
+def calculate_average_satisfaction_of_all_data(last_year_data):
+    all_data = []
+    for one_month_data in last_year_data:
+        all_data.extend(one_month_data['month_data'])
+    return calculate_average_satisfaction(all_data)
+
 
 def main():
     logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
-    measure_id = "8847572"
-    bearer_token = authenticate()
-    data = get_measure_data(bearer_token, measure_id, "2020-03-15", "2020-03-15")
-    avg = calculate_average_satisfaction(data)
-    logging.info("Calculated average: {}".format(avg))
+
+    # get dates for last 12 months
+    fetch_last_12_months_data()
+    # calculate average for the whole period
+
 
 if __name__ == '__main__':
     main()
