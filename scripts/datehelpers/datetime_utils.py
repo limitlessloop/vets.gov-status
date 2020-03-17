9ab900d77f0a19520cf182f623e43db0806fe803
saman.moshafi <saman.moshafi@thoughtworks.com>
2020-03-17 11:49:15 -0400

    [#3208][Saman/Filip] the code to return the start and end dates of the last twelve months

--- scripts/datehelpers/datetime_utils.py
+++ scripts/datehelpers/datetime_utils.py
@@ -1,4 +1,5 @@
 import datetime
+from dateutil import relativedelta
 
 
 def reformat_date(year_month):
@@ -19,3 +20,11 @@ def find_last_full_twelve_months():
     end_date = find_last_day_of_previous_month(datetime.date.today())
     start_date = end_date - datetime.timedelta(days=364)
     return start_date.isoformat(), end_date.isoformat()
+
+
+def find_last_twelve_months():
+    current_date = datetime.datetime.now().date()
+    previous_twelve_months = [find_last_day_of_previous_month(current_date - relativedelta.relativedelta(months=m)) for m in range(0, 12)]
+    return [(last_day_of_month.replace(day=1), last_day_of_month) for last_day_of_month in previous_twelve_months]
+
+
