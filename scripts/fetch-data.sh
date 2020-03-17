8191b898b002ad771e9df218b01e120044d0bd38
Marisa Hoenig <marisa.hoenig@gmail.com>
2020-03-17 15:34:48 -0400

    [#3196][Marisa/Katie] adds getting current date to fetch-data script

--- scripts/fetch-data.sh
+++ scripts/fetch-data.sh
@@ -7,6 +7,11 @@ set -o nounset
 credstash --region ${AWS_DEFAULT_REGION} get vagovanalytics.prod.service_account_credentials > ${GA_SERVICEACCOUNT}
 #python /application/google_analytics/update_data.py
 #python /application/google_analytics/update_counts.py
+#python /application/prometheus/update_from_prometheus.py
+
+echo Running Google Analytics scripts...
 python /application/google_analytics/fetch_ga_data.py
 
-#python /application/prometheus/update_from_prometheus.py
+echo Modifying Last Updated date...
+current_date=$(date "+%B %d, %Y")
+echo "last_updated: $current_date" > application/data/last_updated.yml
