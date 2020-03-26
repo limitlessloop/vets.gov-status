701edcbeadf51d60ce79c6febfc114ff8e29e92f
Shefali Nayak <shefali.nayak@thoughtworks.com>
2020-03-26 18:24:27 -0400

    [#3267][Shefali/Saman] added data integration test to Makefile and Jenkinsfile

--- scripts/fetch-data.sh
+++ scripts/fetch-data.sh
@@ -9,11 +9,15 @@ if [ -n "${CI:-}" ]; then
   export FORESEE_CREDENTIALS=`credstash --region ${AWS_DEFAULT_REGION} get foresee.prod.api.credentials`
 fi
 
-echo Running Google Analytics scripts...
-python -m fetch_data
-
-echo Modifying Last Updated date...
-current_date=$(date "+%B %d, %Y")
-current_time=$(date "+%I:%m %p %Z")
-echo "date: $current_date" > data/last_updated.yml
-echo "time: $current_time" >> data/last_updated.yml
+if [ -z "${INTEGRATION_TEST:-}" ]; then
+  echo Running Google Analytics scripts...
+  python -m fetch_data
+  echo Modifying Last Updated date...
+  current_date=$(date "+%B %d, %Y")
+  current_time=$(date "+%I:%m %p %Z")
+  echo "date: $current_date" > data/last_updated.yml
+  echo "time: $current_time" >> data/last_updated.yml
+else
+  echo Running integration test...
+  python -m test.fetch_data_integration_test
+fi
